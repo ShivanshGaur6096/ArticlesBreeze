0737abbc1c58a70a06b31360d620ec9ae33c9c6e
Shivansh <shivanshgaur96@gmail.com>
2021-12-29 18:24:40 +0530

    Created UI for our Login Page

---
+++ ArticlesBreeze/ArticlesBreeze/LoginTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginTableViewController.swift
+//  ArticlesBreeze
+//
+//  Created by Shivansh Gaur on 29/12/21.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+    
+}
